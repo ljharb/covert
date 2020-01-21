ea75844c6394e8000dac96f7aec4ff60bfd0006f
Jordan Harband <ljharb@gmail.com>
2020-01-21 15:16:28 -0800

    [Tests] add `npm run lint`

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+'use strict';
+
 var parents = require('parents');
 var fs = require('fs');
 var path = require('path');
@@ -8,12 +10,12 @@ var resolve = require('resolve');
 var paths = parents(process.cwd());
 var parts = process.env.PATH.split(path.delimiter);
 var prefix = [];
-var postfix = [ path.join(__dirname, '../node_modules/.bin') ];
+var postfix = [path.join(__dirname, '../node_modules/.bin')];
 var existsSync = fs.existsSync || path.existsSync;
 
 for (var i = 0; i < paths.length; i++) {
     var x = path.join(paths[i], 'node_modules/.bin');
-    if (existsSync(x)) prefix.push(x);
+    if (existsSync(x)) { prefix.push(x); }
 }
 process.env.PATH = prefix.concat(parts).concat(postfix).join(path.delimiter);
 
@@ -21,8 +23,18 @@ var minimist = require('minimist');
 var spawn = require('child_process').spawn;
 
 var argv = minimist(process.argv.slice(2), {
-    boolean: [ 'c', 'h', 'q', 'json' ],
-    alias: { c: 'color', color: 'colors', h: 'help', q: 'quiet' }
+    alias: {
+        c: 'color',
+        color: 'colors',
+        h: 'help',
+        q: 'quiet'
+    },
+    'boolean': [
+        'c',
+        'h',
+        'q',
+        'json'
+    ]
 });
 var vargv = minimist(process.argv.slice(2));
 
@@ -33,10 +45,17 @@ if (args.length === 0 || argv.h || argv.help) {
 }
 
 var coverifyPath = require.resolve('coverify');
-try { coverifyPath = resolve.sync('coverify', { basedir: process.cwd() }) }
-catch (e) {}
+try { coverifyPath = resolve.sync('coverify', { basedir: process.cwd() }); } catch (e) {}
 
 args.unshift('-t', coverifyPath, '--bare', '--node', '--no-bundle-external');
+
+function onexit(name) {
+    return function (code) {
+        if (code === 0) { return; }
+        console.error('non-zero exit code in `' + name + '` command');
+        process.exit(1);
+    };
+}
 
 var browserifyExecutable = process.platform === 'win32' ? 'browserify.cmd' : 'browserify';
 var browserify = spawn(browserifyExecutable, args);
@@ -48,8 +67,8 @@ node.stderr.pipe(process.stderr);
 node.on('exit', onexit('node'));
 
 var cargs = [];
-if (argv.json) cargs.push('--json');
-if (argv.quiet) cargs.push('--quiet');
+if (argv.json) { cargs.push('--json'); }
+if (argv.quiet) { cargs.push('--quiet'); }
 if (vargv.color === undefined && process.stdout.isTTY) {
     cargs.push('--color');
 }
@@ -64,16 +83,7 @@ node.stdout.pipe(coverify.stdin);
 if (argv.json) {
     coverify.stderr.pipe(process.stdout);
     coverify.stdout.pipe(process.stderr);
-}
-else {
+} else {
     coverify.stderr.pipe(process.stderr);
     coverify.stdout.pipe(process.stdout);
-}
-
-function onexit (name) {
-    return function (code) {
-        if (code === 0) return;
-        console.error('non-zero exit code in `' + name + '` command');
-        process.exit(1);
-    };
 }
